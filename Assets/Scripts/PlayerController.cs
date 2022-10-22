fb9def95ee9266187fe9c9f0bdfe449c883ea808
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-10-22 18:03:33 -0500

    feature/Jump-AnimationController
    finish squaring the jump and add your own animations to the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,18 @@ public class PlayerController : MonoBehaviour
     [Header("Date Player")]  
     public float moveSpeed;
     public float forceJump;
+    public bool canDoubleJump;
 
     [Header("Components")]
     public Rigidbody2D rb2D;
+    public Animator anim;
+    public SpriteRenderer sprite;
+
+    [Header("Grounded")]
+
+    private bool isGrounded;
+    public Transform GroundCheckpoint;
+    public LayerMask groundMask;
 
     void Start()
     {
@@ -20,9 +29,38 @@ public class PlayerController : MonoBehaviour
     {
         rb2D.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), rb2D.velocity.y);
 
+        isGrounded = Physics2D.OverlapCircle(GroundCheckpoint.position, .2f, groundMask);
+
+        if (isGrounded)
+        {
+            canDoubleJump = true; 
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, forceJump);
+            if (isGrounded)
+            {
+                rb2D.velocity = new Vector2(rb2D.velocity.x, forceJump); 
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    rb2D.velocity = new Vector2(rb2D.velocity.x, forceJump);
+                    canDoubleJump = false;
+                }
+            }
         }
+
+        if(rb2D.velocity.x < 0)
+        {
+            sprite.flipX = true;
+        }else if(rb2D.velocity.x > 0)
+        {
+            sprite.flipX = false;
+        }
+
+        anim.SetFloat("moveSpeed", Mathf.Abs(rb2D.velocity.x));
+        anim.SetBool("isGrounded", isGrounded); 
     }
 }
