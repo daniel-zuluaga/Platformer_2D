81f0f7b1af64532a74c778b07909a15f14f56240
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-11-05 11:52:42 -0500

    feature/CheckpointsPlayer
    Well, in this commit I added the part of the checkpoints that will have the levels and that the player when entering will have the save position and when he dies he reappears in the last saved position of the checkpoints
    Also rearrange the project so it looks organized and where to look for everything

--- Assets/Scripts/Player/PlayerHealthController.cs
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 
 public class PlayerHealthController : MonoBehaviour
 {
@@ -46,8 +46,7 @@ public class PlayerHealthController : MonoBehaviour
             if (currentHealth <= 0)
             {
                 currentHealth = 0;
-                gameObject.SetActive(false);
-                SceneManager.LoadScene("Level2");
+                LevelManager.instance.RespawnPlayer();
             }
         }
         else
